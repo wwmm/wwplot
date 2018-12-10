08a1af8f2bf67967398f69aec1645aa2c12adb41
Wellington Wallace <wellingtonwallace@gmail.com>
2018-12-10 11:56:57 -0200

    code refactoring

--- WWplot/table.py
+++ WWplot/table.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+from WWplot.import_table import ImportTable
+from WWplot.export_table import ExportTable
+
 from gi.repository import Gdk, Gtk
 
 import os
@@ -94,6 +97,57 @@ class Table():
 
     def onSelectionChanged(self, selection):
         model, self.selected_row = selection.get_selected()
+
+    def onImportTable(self, button):
+        it = ImportTable(self.app.window)
+
+        if it.table is not None:
+            self.liststore.clear()
+
+            if len(it.table.shape) == 2:
+                n_rows, n_cols = it.table.shape
+
+                for row in it.table:
+                    self.liststore.append([row[0], row[1], row[2], row[3]])
+
+            elif len(it.table.shape) == 1:
+                for row in it.table:
+                    self.liststore.append([row, 0, 0, 0])
+
+            self.app.updatePlot()
+            # self.clear_fitlog()
+
+    def onExportTable(self, button):
+        n_rows = len(self.liststore)
+
+        if n_rows > 0:
+            if self.app.do_histogram is False:
+                n_cols = self.liststore.get_n_columns()
+            else:
+                n_cols = 1
+
+            table = np.empty([n_rows, n_cols], dtype=float)
+
+            row_iter = self.liststore.get_iter_first()
+
+            idx = 0
+
+            while row_iter is not None:
+                c0, c1, c2, c3 = self.liststore.get(row_iter, 0, 1, 2, 3)
+
+                if n_cols == 4:
+                    table[idx, 0] = c0
+                    table[idx, 1] = c1
+                    table[idx, 2] = c2
+                    table[idx, 3] = c3
+                elif n_cols == 1:
+                    table[idx, 0] = c0
+
+                idx = idx + 1
+
+                row_iter = self.liststore.iter_next(row_iter)
+
+            ExportTable(self.app.window, table)
 
     def onKeyPressed(self, widget, event):
         if event.keyval == Gdk.keyval_from_name('c'):
