dc2d7b38ac760c9d3985f33a6ee599647378fccf
wwmm <wellingtonwallace@gmail.com>
2024-04-25 12:16:57 -0300

    adding github actions configuration

--- gen_win_exe.bat
+++ gen_win_exe.bat
@@ -1,9 +1,9 @@
-pyinstaller wwplot.in ^
+C:\users\wallace\AppData\Local\Programs\Python\Python312\Scripts\pyinstaller wwplot.in ^
 --onefile ^
 --hidden-import=numpy.random.common ^
 --hidden-import=numpy.random.bounded_integers ^
 --hidden-import=numpy.random.common ^
 --hidden-import=numpy.random.entropy ^
---hidden-import=PySide2.QtXml ^
+--hidden-import=PySide6.QtXml ^
 --hidden-import=matplotlib ^
 --add-data="WWplot/ui;WWplot/ui"
